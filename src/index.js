e3c5694a3228d8841cf9a79690aecf635e7bee7f
Justin Lafferty <lafferjm@gmail.com>
2019-05-18 14:22:05 -0500

    scrolling tilemap with camera

--- src/index.js
+++ src/index.js
@@ -12,10 +12,39 @@ var config = {
     }
 };
 
+var controls;
+
 new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+    this.load.image('tiles', require('../assets/tilesets/tuxmon-sample-32px-extruded.png'));
+    this.load.tilemapTiledJSON('map', require('../assets/tilemaps/tuxemon-town.json'));
+}
 
-function create() {}
+function create() {
+    const map = this.make.tilemap({ key: 'map' });
 
-function update() {}
+    const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+
+    const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+    const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+    const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
+
+    const camera = this.cameras.main;
+
+    const cursors = this.input.keyboard.createCursorKeys();
+    controls = new Phaser.Cameras.Controls.FixedKeyControl({
+        camera: camera,
+        left: cursors.left,
+        right: cursors.right,
+        up: cursors.up,
+        down: cursors.down,
+        speed: 0.5
+    });
+
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+}
+
+function update(time, delta) {
+    controls.update(delta);
+}
